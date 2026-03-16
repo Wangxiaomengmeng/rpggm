787223ae59287d7812a3eac84c172edbc9d056d4
myname <test@example.com>
2026-03-16 13:19:20 +0800

    new

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,43 +1,45 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace "com.example.rpg_game" // 替换为你的包名
-    compileSdkVersion 34
+    namespace = "com.example.rpg_game"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.example.rpg_game" // 必须和 Firebase 控制台填写的包名一致
-        minSdkVersion 21 // Firebase 要求 ≥19，建议 21
-        targetSdkVersion 34
-        versionCode flutterVersionCode.toInteger()
-        versionName flutterVersionName
+        applicationId = "com.example.rpg_game"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
     }
 
     buildTypes {
         release {
-            signingConfig signingConfigs.debug // 测试用，正式打包需配置签名
-            minifyEnabled false
+            signingConfig = signingConfigs.getByName("debug")
+            minifyEnabled = false
         }
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        compilerOptions {
+            jvmTarget.set("1.8")
+        }
     }
 }
 
 flutter {
-    source "../.."
+    source("../..")
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0")
 }
